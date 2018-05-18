5e9331847bdfb0de3dcf88e06d7db2fce73c94ba
Cesar <cesar_lima02@hotmail.com>
2018-05-17 23:24:04 -0300

    second site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/server');
 
-var server = app.listen(1337, function () {
+var server = app.listen(3000, function () {
     console.log('Servidor ON');
 })
 
--- app/public/js/chat/socket.js
+++ app/public/js/chat/socket.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:1337'); // io usa .connect agora
+var socket = io.connect('http://localhost'); // io usa .connect agora
 
 $('document').ready(() => {
     socket.emit(
